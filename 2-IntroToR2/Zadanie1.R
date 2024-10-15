9f7e6ebdd4bb3c6e1b9ce4c865d6ea067257b6a3
FSzew <franek.szewczak@gmail.com>
2024-10-15 11:02:36 +0200

    Add files via upload

--- 2-IntroToR2/Zadanie1.R
+++ 2-IntroToR2/Zadanie1.R
@@ -1,15 +1,29 @@
+# Zadania do wspólnego rozwiązania
 
+## Zadanie 1: Zapoznanie się z RStudio
+
+**Cel:** Poznanie interfejsu i podstawowych funkcji RStudio.
+
+### Instrukcje i rozwiązanie:
+
+1.  **Uruchom RStudio** na swoim komputerze.
+
+2.  **Zapoznaj się z interfejsem użytkownika:**
   
-  #----------------FUNKCJE POSZCZEGÓLNYCH PANELI----------- 
+  3.  **Stwórz nowy skrypt R:**
+  
+  4.  **W skrypcie napisz komentarze** opisujące funkcje poszczególnych paneli.
 
-  # Source (Edytor skryptów) służy do pisania skryptów, a funkcje nie są automatycznie wykonywane
-  # Console (Konsola) służy do...
+  #Source (edytor skryptu)- służy do pisania skryptów, a funkcje nie są automatycznie wykonywane
+  #Console (konsola) służy- do wyświetlania już uruchomionych funkcji
+  #Enviroment (środowisko)- wyświetla wszystkie przypisane wartości
+  #Output panel- służy do otwierania oraz wyświetlania plików z komputera
 
+5.  **Znajdź w RStudio ustawienia** i **zmień motyw kolorystyczny** na taki, który najbardziej Ci odpowiada.
+  #Motyw Chaos
 
-#-------------motyw kolorystyczny------------
-
-# wybrałam motyw 
-  6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+6.  **Napisz w komentarzu**, jaki motyw wybrałeś/aś i dlaczego.
+  #Motyw Chaos charakteryzuje się wysokim kontrastem, który nandaje skryptowi dużą przejrzystość
 
 7.  **Zapisz skrypt** jako `zadanie1.R` w swoim folderze.
 
