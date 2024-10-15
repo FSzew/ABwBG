697fa167fe1818c74d4ff00918f0e89cb4ce245f
FSzew <franek.szewczak@gmail.com>
2024-10-15 12:06:45 +0200

    Add files via upload

---
+++ 2-IntroToR2/Zadanie2.R
@@ -0,0 +1,44 @@
+## Zadanie 2: Instalacja i użycie pakietów
+
+**Cel:** Nauka instalowania i używania pakietów w R
+
+### Instrukcje i rozwiązanie:
+
+1.  **Zainstaluj pakiet `ggplot2`.**
+
+    ``` r
+    install.packages("ggplot2")
+    ```
+
+2.  **Załaduj pakiet.**
+
+    ``` r
+    library(ggplot2)
+    ```
+
+3.  **Sprawdź dokumentację pakietu.**
+
+    ``` r
+    ?ggplot2
+    ```
+
+4.  **Zainstaluj i załaduj** dodatkowy pakiet, który Cię zainteresuje. Listę pakietów w repozytorium CRAN wraz z opisami znajdziesz tutaj: <https://cran.r-project.org/web/packages/available_packages_by_name.html>
+
+    ``` r
+    install.packages("plotly")
+    library(plotly)
+    ```
+
+5.  **Napisz krótki opis** w komentarzu.
+
+    ``` r
+    #PAKIET ADAPTS
+    #Tools to construct (or add to) cell-type signature matrices using flow sorted or single cell samples and deconvolve bulk gene expression data.
+    #Useful for assessing the quality of single cell RNAseq experiments, estimating the accuracy of signature matrices, and determining cell-type spillover.
+    #Please cite: Danziger SA et al. (2019) ADAPTS: Automated Deconvolution Augmentation of Profiles for Tissue Specific cells <doi:10.1371/journal.pone.0224693>.
+    
+    ```
+
+6.  **Zapisz skrypt** jako `zadanie2.R` i prześlij go do swojego repozytorium.
+
+------------------------------------------------------------------------??
